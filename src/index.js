a878607ab8ed9e9a2ef0e842b4492b7a961722eb
Bad-Raider <stanislav.gusiev@gmail.com>
2023-04-01 22:43:34 +0300

    hw-11
    instal oll labirary
    created js file
    created handleBars file
    created html file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ import markupGallery from './templates/markupGallary.hbs';
 import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import axios from 'axios';
 
 
+
+const refs = {
+    formEL: document.querySelector(".search-form"),
+    inputEl: document.querySelector("input[name='searchQuery']"),
+    btnSubmit: document.querySelector("button[type='submit']"),
+    gallaryEl: document.querySelector(".gallery"),
+    btnLoadMoreEl: document.querySelector(".load-more"),
+}
 
 
