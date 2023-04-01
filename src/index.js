71b19779097b48479b9b38e0ba2f93a39950dc30
Bad-Raider <stanislav.gusiev@gmail.com>
2023-04-01 21:00:57 +0300

    start-project #4

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import './css/styles.css';
+import './js/fetch';
+import markupGallery from './templates/markupGallary.hbs';
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 
