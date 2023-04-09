33df52729f134956dcdc37aa9d0855d66eaf2681
Bad-Raider <stanislav.gusiev@gmail.com>
2023-04-09 20:47:42 +0300

    deleted
    markup & CSS & js code by button "load more"

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './css/styles.css';
 import './css/gallery.css';
 import './css/form.css';
-// import './css/btnLoadMore.css';
 import './css/btnBackToTop.css';
 import './js/fetch';
 import './js/btnUpTop';
@@ -18,7 +17,6 @@ const refs = {
     inputEl: document.querySelector("input[name='searchQuery']"),
     btnSubmit: document.querySelector("button[type='submit']"),
     gallaryEl: document.querySelector(".gallery"),
-    // btnLoadMoreEl: document.querySelector(".load-more"),
 };
 
 let page = 1;
@@ -27,8 +25,6 @@ let inputValue = "";
 let simpleLightBox; 
 
 
-// // hide btn "Loade More", when you first start searching photo
-// refs.btnLoadMoreEl.classList.add("is-hiden");
 // Searching photo
 refs.formEL.addEventListener("submit", handleSearchPhotoBySubmitForm);
 
@@ -55,12 +51,9 @@ function handleSearchPhotoBySubmitForm(e) {
             /* checking query, if he empty, then show a message and 
             don`t show button "load More"*/  
             if (hits.length === 0) {
-                // refs.btnLoadMoreEl.classList.add("is-hiden");
                 destroyMarkup();
                 return photoSearchError();
             };
-            // show button "load More"
-            //  refs.btnLoadMoreEl.classList.remove("is-hiden");
 
             destroyMarkup();
             showMessageAboutAllPhoto(totalHits);           
@@ -70,87 +63,16 @@ function handleSearchPhotoBySubmitForm(e) {
             if there is no data, show a message about end search + 
             don`t show button "load More"*/
             if (page >= totalPages) { 
-                // refs.btnLoadMoreEl.classList.add("is-hiden");
                 showInfoMessageEndSearch()
             };
-
         })
         .catch(() => {
             photoSearchError();
         })
 };
 
-// Load More photo
-// refs.btnLoadMoreEl.addEventListener("click", handleBtnClick);
-
-// handler btn load more photo 
-// function handleBtnClick() {
-// // counter pages after loadning more photo
-//     page += 1;
-// // updating simpleLightBox after next restart pages  
-//     simpleLightBox.destroy();
-
-//         fetchSearchPhoto(inputValue, page, perPage)
-//         .then(({totalHits, hits}) => {           
-//             // Total number of pages
-//             const totalPages = Math.ceil(totalHits / perPage)
-//             /* check the data received from the backend database, 
-//             if there is no data, show a message about end search + 
-//             don`t show button "load More"*/
-//             if (page > totalPages) {
-//                 refs.btnLoadMoreEl.classList.add("is-hiden");
-//                 showInfoMessageEndSearch()
-//             }    
-//             createMarkup(hits);
-//             createSimpleLightbox();
-//             scrollingPages();
-//         })
-//         .catch((error) => {
-//             console.log(error)
-//         })
-// };
-
-
-
-// All functions, which 
-function createMarkup(data) {
-    refs.gallaryEl.insertAdjacentHTML("beforeend", markupGallery(data));
-};
-
-function destroyMarkup() {
-    refs.gallaryEl.innerHTML = "";
-}
-
-function photoSearchError() {
-    Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");    
-};
-
-function showMessageAboutAllPhoto(totalHits) {
-    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`)
-}
-function showInfoMessageEndSearch() {
-    Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")
-}
-
-function createSimpleLightbox () {
-    simpleLightBox = new SimpleLightbox('.gallery a').refresh();
-};
-
-// function scrollingPages() {
-//     const { height: cardHeight } = refs.gallaryEl
-//         .firstElementChild.getBoundingClientRect();
-
-//     window.scrollBy({
-//     top: cardHeight * 2,
-//     behavior: "smooth",
-// });  
-// };
-
-
 // infinity scroll
-
 //  option infinity scroll
-
 const options = {    
     rootMargin: '500px',
 };
@@ -187,4 +109,29 @@ const callback = (entries) => {
 const observer = new IntersectionObserver(callback, options);
 
 observer.observe(document.querySelector('#scrollArea'));
+
+// All functions, which 
+function createMarkup(data) {
+    refs.gallaryEl.insertAdjacentHTML("beforeend", markupGallery(data));
+};
+
+function destroyMarkup() {
+    refs.gallaryEl.innerHTML = "";
+}
+
+function photoSearchError() {
+    Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");    
+};
+
+function showMessageAboutAllPhoto(totalHits) {
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`)
+}
+function showInfoMessageEndSearch() {
+    Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")
+}
+
+function createSimpleLightbox () {
+    simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+};
+
 
